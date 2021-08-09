265dc4d5cad6c0b8f5e3042ba8ffc7079c7eaeb5
Emad Motameni <emad.motameni76@gmail.com>
2021-08-09 10:41:57 +0430

    add Bubble Sort & Selection Sort

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+// generate an array with 30 random elements, with random values from 0 to 99
+var numbers = [];
+while(numbers.length <= 30){
+    numbers.push(Math.floor(Math.random() * 100));
+}
+console.log("Unsorted array: \n", JSON.stringify(numbers), "\n");
+
+// bubble sort
+for(let i = numbers.length; i > 1; i--){
+    var leftElement = 0, rightElement = 1;
+    while(rightElement != i){
+        if(numbers[leftElement] > numbers[rightElement]){
+            [numbers[leftElement], numbers[rightElement]] = [numbers[rightElement], numbers[leftElement]];
+        }
+        leftElement++;
+        rightElement++;
+    }
+}
+console.log("Sorted array with Bubble Sort: \n", JSON.stringify(numbers));
---
+++ selectionSort.js
@@ -0,0 +1,22 @@
+// generate an array with 30 random elements, with random values from 0 to 99
+var numbers = [];
+while(numbers.length <= 30){
+    numbers.push(Math.floor(Math.random() * 100));
+}
+console.log("Unsorted array: \n", JSON.stringify(numbers), "\n");
+
+// selection sort
+for(var i = 0; i < numbers.length; i++){
+    var min = numbers[i], minElement, j = i, temp;
+    while(j < numbers.length){
+        if(numbers[j] < min){
+            min = numbers[j];
+            minElement = j;
+        }
+        j++;
+    }
+    temp = numbers[i];
+    numbers[i] = min;
+    numbers[minElement] = temp;
+}
+console.log("Sorted array with Selection Sort: \n", JSON.stringify(numbers));
